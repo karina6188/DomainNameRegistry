b5e32d522218f085799188a565b840aad3094ef0
Karina Chen <karina6188@gmail.com>
2020-05-13 15:14:15 -0700

    validate domain name and period range

--- DomainRegistry/Pages/Index.cshtml.cs
+++ DomainRegistry/Pages/Index.cshtml.cs
@@ -12,9 +12,35 @@ namespace DomainRegistry.Pages
     public class IndexModel : PageModel
     {
         public IConfiguration Configuration { get; set; }
+
+        [BindProperty]
+        public RegisterInput Input { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+        public class RegisterInput
+        {
+            [StringLength(30, MinimumLength = 10)]
+            [RegularExpression(@"^[a-zA-Z0-9""'\s-]*$")]
+            [Required]
+            [Display(Name = "Domain Name:")]
+            public string DomainName { get; set; }
+
+            [Range(1, 5)]
+            [Required]
+            [Display(Name = "Period of Registration(years):")]
+            public int Period { get; set; }
+
+            [Required]
+            [Display(Name = "Verification Provider:")]
+            public string Provider { get; set; }
+
+            [Required]
+            [Display(Name = "Contact ID:")]
+            public string ContactID { get; set; }
         }
     }
 }
