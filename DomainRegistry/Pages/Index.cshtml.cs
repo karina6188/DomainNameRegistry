0d047e272ca198c946d9aea10803067888e58748
Karina Chen <karina6188@gmail.com>
2020-05-13 14:54:10 -0700

    Set up Register Form and validate input

--- DomainRegistry/Pages/Index.cshtml.cs
+++ DomainRegistry/Pages/Index.cshtml.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Configuration;
 
 namespace DomainRegistry.Pages
 {
     public class IndexModel : PageModel
     {
+        public IConfiguration Configuration { get; set; }
         public void OnGet()
         {
 
