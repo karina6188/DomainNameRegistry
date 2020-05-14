53226a65f7268e884c936223d22613a416ebcc01
Karina Chen <karina6188@gmail.com>
2020-05-13 17:32:37 -0700

    set up domain interface and service methods

---
+++ DomainRegistry/Models/Interfaces/IDomain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DomainRegistry.Models.Interfaces
+{
+    public interface IDomain
+    {
+        Task SaveDomainAsync(Domain domain);
+
+        Task GetDomainByNameAsync(string domainName);
+
+        Task UpdateDomain(Domain domain);
+    }
+}
--- DomainRegistry/Pages/Index.cshtml.cs
+++ DomainRegistry/Pages/Index.cshtml.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.Extensions.Configuration;
+using DomainRegistry.Models;
+using DomainRegistry.Data;
 
 namespace DomainRegistry.Pages
 {
@@ -16,15 +18,40 @@ namespace DomainRegistry.Pages
         [BindProperty]
         public RegisterInput Input { get; set; }
 
+        private DomainDbContext _context;
+
+        public IndexModel(DomainDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
 
         }
 
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var domain = new Domain
+            {
+                DomainName = Input.DomainName,
+                Period = Input.Period,
+                Provider = Input.Provider,
+                ContactID = Input.ContactID
+            };
+
+            var result = await _context.
+            
+        }
+
         public class RegisterInput
         {
             [StringLength(30, MinimumLength = 10)]
-            [RegularExpression(@"^[a-zA-Z0-9""'\s-]*$")]
+            [RegularExpression(@"^[a-zA-Z0-9""'\s-]*$", ErrorMessage = "The field Domain Name: can only contain characters and numbers")]
             [Required]
             [Display(Name = "Domain Name:")]
             public string DomainName { get; set; }
