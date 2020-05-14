0d37a55bc12f1539574ec98225f4ef8e87a5d806
Karina Chen <karina6188@gmail.com>
2020-05-13 21:11:35 -0700

    add update operation to domain name

---
+++ DomainRegistry/Pages/Renew.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DomainRegistry.Models;
+using DomainRegistry.Models.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DomainRegistry.Pages
+{
+    public class RenewModel : PageModel
+    {
+        private readonly IDomain _domain;
+
+        public RenewModel(IDomain domain)
+        {
+            _domain = domain;
+        }
+
+        public Domain Domain { get; set; }
+
+        public async Task OnGetAsync(int id)
+        {
+            Domain = await _domain.GetDomainByDomainIdAsync(id);
+        }
+    }
+}
